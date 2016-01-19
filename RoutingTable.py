6ad49be461fe5f8529bc03c710f089743477d19f
Stefano Franz <stefano.franz@byhiras.com>
2016-01-19 14:40:15 +0000

    implement a binary search for node buckets

--- RoutingTable.py
+++ RoutingTable.py
@@ -1,4 +1,5 @@
 import math
+import os
 
 from Swarm import Node
 
@@ -13,11 +14,11 @@ def _strxor(a, b):
 
 
 # CONTRACT IS, IF COMPARATOR EVALUATES TO > 1, THE ITEM WE JUST ASKED ABOUT IS LARGER THAN THE ONE WE CURRIED IN
-def search_sorted_list(items, comparator, startIdx, endIdx):
+def fucksake_why_isnt_this_std_lib_search_sorted_list(items, comparator, startIdx, endIdx):
     if startIdx == endIdx:
-        return None
-
-    idx = startIdx + int(math.ceil((endIdx - startIdx) / 2))
+        return items[startIdx] if comparator(items[startIdx]) == 0 else None, startIdx
+    else:
+        idx = startIdx + int(math.ceil((endIdx - startIdx) / 2))
 
     while True:
         # the comparator has a curried item in it (ie, the item we are looking for)
@@ -25,18 +26,19 @@ def search_sorted_list(items, comparator, startIdx, endIdx):
 
         if comparison_result == 0:
             # SUCCESS, we have found the thing
-            return items[idx]
+            # lets use the ONE return point defined above - inefficient I know.
+            return fucksake_why_isnt_this_std_lib_search_sorted_list(items, comparator, idx, idx)
         elif comparison_result > 0:
             new_start_idx = idx + 1
             new_end_idx = endIdx
-            return search_sorted_list(items, comparator,
-                                      new_start_idx, new_end_idx)
+            return fucksake_why_isnt_this_std_lib_search_sorted_list(items, comparator,
+                                                                     new_start_idx, new_end_idx)
         elif comparison_result < 0:
             new_start_idx = startIdx
             new_end_idx = idx - 1
 
-            return search_sorted_list(items, comparator,
-                                      new_start_idx, new_end_idx)
+            return fucksake_why_isnt_this_std_lib_search_sorted_list(items, comparator,
+                                                                     new_start_idx, new_end_idx)
 
 
 class Bucket(object):
@@ -48,7 +50,7 @@ class Bucket(object):
         self.max_id = max_id
 
     def has_capacity(self):
-        return len(self.nodes) >= Bucket.CAPACITY
+        return not len(self.nodes) >= Bucket.CAPACITY
 
     def add_node(self, node_to_add):
         self.nodes.append(node_to_add)
@@ -56,7 +58,7 @@ class Bucket(object):
     @staticmethod
     def build_comparator_for_node(id_to_fit_into_bucket):
         def id_based_bucket_comparator(bucket_to_check):
-            if bucket_to_check.min_id <= id_to_fit_into_bucket < bucket_to_check.max_id:
+            if bucket_to_check.min_id <= id_to_fit_into_bucket <= bucket_to_check.max_id:
                 # the id is within the range of this bucket
                 return 0
             else:
@@ -67,29 +69,36 @@ class Bucket(object):
 
 class RoutingTable(object):
     MAX_NUMBER_OF_BUCKETS = int(pow(2, 16))
-    MAX_ID = 32
+    MAX_ID = long(pow(2, 160)) - 1
     MIN_ID = 0
 
     def __init__(self):
         self.buckets = [Bucket(RoutingTable.MIN_ID, RoutingTable.MAX_ID / 2),
-                        Bucket(RoutingTable.MAX_ID / 2 + 1, RoutingTable.MAX_ID)]
+                        Bucket(RoutingTable.MAX_ID / 2, RoutingTable.MAX_ID)]
 
     def add_node(self, node):
         bucket = self.find_node_bucket(node)
         bucket.add_node(node)
 
     def find_node_bucket(self, node):
-        return search_sorted_list(self.buckets, Bucket.build_comparator_for_node(node.long_id), 0, len(self.buckets))
+        search_result = fucksake_why_isnt_this_std_lib_search_sorted_list(self.buckets,
+                                                                          Bucket.build_comparator_for_node(
+                                                                              node.long_id),
+                                                                          0, len(self.buckets) - 1)
+        found_bucket = search_result[0]
+        if not found_bucket:
+            # we did not find a bucket, something is very wrong
+            # at any point there should be buckets covering the whole space
+            raise KeyError("Could not find a bucket for node_id: " + node.long_id)
 
-    def __repr__(self):
-        pass
+        return search_result
 
 
 table = RoutingTable()
 
-for i in range(1, 32):
-    node = Node(("123.1.2.3", 456), str(i))
-    print "adding + " + str(node)
+for i in range(0, pow(2, 16)):
+    key = os.urandom(20)  # 20 random bytes == 160 bits
+    node = Node(("123.1.2." + str(i), 456), key)
     table.add_node(node)
 
-print table
+print float(len(table.buckets[0].nodes)) / len(table.buckets[1].nodes)
--- Swarm.py
+++ Swarm.py
@@ -1,4 +1,21 @@
-import binascii
+import math
+
+
+def fucksake_why_isnt_this_std_lib_convert_eight_bit_string_to_number(string, base):
+    # assume big endian
+    value = 0
+    power = len(string) - 1
+    for character in string:
+
+        if ord(character) >= base:
+            raise ArithmeticError("Encountered value: " + str(character) +
+                                  " which is larger than " + str(base))
+
+        value_of_this_index = math.pow(base, power) * ord(character)
+        value += value_of_this_index
+
+        power -= 1
+    return long(value)
 
 
 class Node(object):
@@ -6,9 +23,7 @@ class Node(object):
         self.address = address
         if node_id:
             self.id = node_id
-
-            hex_id = binascii.b2a_hex(node_id)
-            self.long_id = long(hex_id, base=16)
+            self.long_id = fucksake_why_isnt_this_std_lib_convert_eight_bit_string_to_number(node_id, 256)
 
     def __repr__(self):
         return "Node " + str(self.id) + " @ " + str(self.address)
